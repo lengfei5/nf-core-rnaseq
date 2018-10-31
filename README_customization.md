b0c511945a8856ee3c50d2994d0b6864da192b9c
jingkui.wang <jingkui.wang@imp.ac.at>
2018-10-31 10:15:23 +0100

    backup and start to customize the pipeline

---
+++ README_customization.md
@@ -0,0 +1,8 @@
+# rnaseq_nf 
+
+This is a rnaseq data processing pipeline based on the 
+https://github.com/ewels/nf-core-rnaseq which is really nice nf pipeline.
+
+Some customized modification has been done to adapt the IMP/IMBA slurm HPC  
+
+
---
+++ test.sh
@@ -0,0 +1,13 @@
+##############
+# the test script for rnaseq nf 
+# https://github.com/lengfei5/nf-core-rnaseq/blob/master/docs/configuration/adding_your_own.md
+# 
+##############
+fqs='/scratch/jiwang/Ariane_R6875/ngs_raw/*.fastq'
+star_index='/groups/cochella/jiwang/Genomes/C_elegans/WBcel235/index_4star'
+GTF='/groups/cochella/jiwang/annotations/Caenorhabditis_elegans.WBcel235.88.gtf'
+output='/scratch/jiwang/Ariane_R6875/results'
+workdir='/scratch/jiwang/Ariane_R6875/work'
+sequence='/groups/cochella/jiwang/Genomes/C_elegans/WBcel235/Sequence_Annotation/Caenorhabditis_elegans.WBcel235.dna.toplevel.fa'
+
+nextflow run /groups/cochella/jiwang/scripts/nf-core-rnaseq/ -profile slurm --reads '/scratch/jiwang/Ariane_R6875/ngs_raw/*.fastq' --singleEnd --unstranded --star_index $star_index --fasta $sequence --gtf $GTF --outdir $output -w $workdir --skip_edger --skip_preseq --skip_genebody_coverage --saveAlignedIntermediates 
