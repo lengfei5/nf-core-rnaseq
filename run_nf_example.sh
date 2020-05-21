8e341131c628069e847860567fb9aefc2e2a1bda
jingkui.wang <jingkui.wang@imp.ac.at>
2020-05-21 11:29:17 +0200

    update for ii2 module load

--- run_nf_example.sh
+++ run_nf_example.sh
@@ -3,11 +3,33 @@
 # https://github.com/lengfei5/nf-core-rnaseq/blob/master/docs/configuration/adding_your_own.md
 # 
 ##############
-fqs='/scratch/jiwang/Ariane_R6875/ngs_raw/FASTQs/*.fastq'
-output='/scratch/jiwang/Ariane_R6875/results'
-workdir='/scratch/jiwang/Ariane_R6875/work'
+# bam to fastq
+#~/scripts/ngs_tools/bam2fastq.sh
 
-star_index='/groups/cochella/jiwang/Genomes/C_elegans/WBcel235/index_4star'
-GTF='/groups/cochella/jiwang/annotations/Caenorhabditis_elegans.WBcel235.88.gtf'
+# run rnaseq-nf
+ml load nextflow/19.01.0
+ml load singularity/3.4.1
 
-nextflow run /groups/cochella/jiwang/scripts/rnaseq_nf -profile slurm --reads '/scratch/jiwang/Ariane_R6875/ngs_raw/*.fastq' --singleEnd --unstranded --star_index $star_index --gtf $GTF --max_IntronL 50000  --outdir $output -w $workdir --skip_edger --skip_preseq --saveAlignedIntermediates --skip_genebody_coverage
+fqs="/groups/cochella/jiwang/Projects/Aleks/scRNAseq_published_dataSets/scRNAseq_Tintori/Raw/ngs_raw/FASTQs/*.gz"
+output="$PWD/nf_out_scRNA"
+workdir="$PWD/work"
+
+star_index="/groups/cochella/jiwang/Genomes/C_elegans/WBcel235/index_4star"
+GTF="/groups/cochella/jiwang/annotations/Caenorhabditis_elegans.WBcel235.88.gtf"
+
+nextflow run /groups/cochella/jiwang/scripts/rnaseq_nf -profile slurm \
+--reads "$fqs" \
+--singleEnd \
+--unstranded \
+--star_index "$star_index" \
+--gtf "$GTF" \
+--max_IntronL 50000 \
+--outdir "$output" \
+-w "$workdir" \
+--skip_preseq \
+--saveAlignedIntermediates \
+--skip_genebody_coverage \
+--skip_dupradar \
+--skip_edger \
+--sampleLevel \
+--skip_multiqc
